ed2a7bf5b493cf5543e95086451ebbbd27ca78c3
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-16 14:31:15 +0300

    configuring the db table

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.conf import settings
+from django.contrib.contenttypes import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 # Create your models here.
+User=settings.AUTH_USER_MODEL
+class ObjectViewed(models.Model):
+    # user instance
+    user=models.ForeignKey(blank=True, null=True),
+    # user,product, address, location  
+    content_type= models.ForeignKey(ContentType)
+    # user id, product id,address id
+    object_id =models.PositiveIntegerField() 
+    # object instance
+    content_object= GenericForeignKey('content_type', 'object_id')
+    timestamp=models.DateTimeField(auto_now_add=True)
+    ip_address=models.CharField(max_length=220, blank=True,null=True)
+
+    def __str__(self):
+        return "%s viewed %s" %(self.content_object,self.timestamp)
+
+    class Meta:
+        ordering=['-timestamp']
+        verbose_name = 'Object Viewed'
+        verbose_name_plural = 'Objects Viewed'
+    
