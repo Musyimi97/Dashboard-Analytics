34ce94accd48f675f6f3385c057b221501e0c871
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-16 14:45:28 +0300

    its now okay

--- analytics/models.py
+++ analytics/models.py
@@ -1,26 +1,24 @@
-from django.db import models
+# analytics.models.py
 from django.conf import settings
+from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-User=settings.AUTH_USER_MODEL
+
+User= settings.AUTH_USER_MODEL
+
 
 class ObjectViewed(models.Model):
-    # user instance
-    user=models.ForeignKey(User, blank=True, null=True),
-    # user,product, address, location  
-    content_type= models.ForeignKey(ContentType)
-    # user id, product id,address id
-    object_id =models.PositiveIntegerField() 
-    # object instance
-    content_object= GenericForeignKey('content_type', 'object_id')
-    timestamp=models.DateTimeField(auto_now_add=True)
-    ip_address=models.CharField(max_length=220, blank=True,null=True)
+    user            = models.ForeignKey(User, blank=True, null=True ,on_delete=models.CASCADE)
+    content_type    = models.ForeignKey(ContentType, on_delete=models.SET_NULL, null=True)
+    object_id       = models.PositiveIntegerField()
+    ip_address      = models.CharField(max_length=120, blank=True, null=True)
+    content_object  = GenericForeignKey('content_type', 'object_id')
+    timestamp       = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return "%s viewed %s" %(self.content_object,self.timestamp)
+    def __str__(self, ):
+        return "%s viewed: %s" %(self.content_object, self.timestamp)
 
     class Meta:
-        ordering=['-timestamp']
+        ordering = ['-timestamp']
         verbose_name = 'Object Viewed'
         verbose_name_plural = 'Objects Viewed'
-    
