a51c3dc7d9079722f48e41a3e9c75779761efa6b
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-16 14:36:14 +0300

    models look good

--- analytics/models.py
+++ analytics/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-# Create your models here.
 User=settings.AUTH_USER_MODEL
+
 class ObjectViewed(models.Model):
     # user instance
     user=models.ForeignKey(blank=True, null=True),
