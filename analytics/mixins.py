e0533027499fa22665422a67636d0e9e64916493
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-16 15:48:11 +0300

    mixins updated

--- analytics/mixins.py
+++ analytics/mixins.py
@@ -1,1 +1,9 @@
 from .signals import object_viewed_signal
+
+
+class ObjectViewedMixin(object):
+    def get_context_data(self, *args, **kwargs):
+        context= super(ObjectViewedMixin, self).get_context_data(*args, **kwargs)
+        request=self.request
+        instance =context ['object']
+        object_viewed_signal.send(instance.__class__, request='request',instance='instance')
